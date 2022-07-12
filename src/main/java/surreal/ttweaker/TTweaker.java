62e59bd1e9cefe29745e73bf3aad5895714cad0d
ISurrealI <merhabalarexoria@gmail.com>
2022-07-13 01:25:04 +0300

    Don't ask this

--- src/main/java/surreal/ttweaker/TTweaker.java
+++ src/main/java/surreal/ttweaker/TTweaker.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.Logger;
 public class TTweaker {
     public static final String MODID = "ttweaker";
     public static final String NAME = "TTweaker";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
 
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
